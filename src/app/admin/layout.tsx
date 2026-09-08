6c1463af8ae88dde0cb48516577ba4e76ef8e9f3
Suin <ssi462530@gmail.com>
2026-09-08 20:54:08 +0900

    fix

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,7 @@
 // app/admin/layout.tsx
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
-import { ADMIN_EMAIL } from "@/costants/admin";
+import { ADMIN_EMAILS } from "@/constants/admin";
 
 export default async function AdminLayout({
   children,
@@ -14,7 +14,7 @@ export default async function AdminLayout({
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (!user || !ADMIN_EMAILS.includes(user.email)) {
+  if (!user || !user.email || !ADMIN_EMAILS.includes(user.email)) {
     redirect("/");
   }
 
