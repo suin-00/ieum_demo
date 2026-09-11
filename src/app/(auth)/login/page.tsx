5aabaf5563b74570ac05dc791b169d512359cd80
Suin <ssi462530@gmail.com>
2026-09-11 19:12:13 +0900

    로그인 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -13,7 +13,8 @@ export default function LoginPage() {
         priority
       />
       {/* 💡 크기를 대폭 키운 유리 카드 폼 (max-w-xl, p-12) */}
-      <div className="w-full max-w-2xl rounded-3xl bg-white/60 backdrop-blur-md px-16 py-60 shadow-2xl border border-white/40">
+      <div className="w-[90%] sm:w-[460px] lg:w-[28vw] max-w-lg min-w-[380px] rounded-3xl bg-white/50 backdrop-blur-md px-12 py-14 lg:py-20 shadow-2xl border border-white/40 flex flex-col justify-center">
+        {" "}
         <LogIn />
       </div>
     </main>
